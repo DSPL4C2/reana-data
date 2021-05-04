8dd400c17ce8203185e2ab443f1b512ece0a5303
brenoafb <breno.afb@gmail.com>
2021-05-04 00:34:57 -0300

    Add option to change yscale and output path

--- display.py
+++ display.py
@@ -7,7 +7,7 @@ from plotting import *
 from stats import *
 from tabulator import *
 
-def get_pairwise_graphs(spl, labels, xoffset=0):
+def get_pairwise_graphs(spl, labels, xoffset=0, yscale='log', output_path='results'):
    display(Markdown('# {}'.format(spl)))
    rt_filenames = [
        'csv/running_time/totalTime{}{}.csv'.format(spl, label) for label in labels]
@@ -24,8 +24,8 @@ def get_pairwise_graphs(spl, labels, xoffset=0):
        xlabel = 'Evolution'
        ylabel = 'Running Time (s)'
        make_line_graph(rt_df, spl, pair, title=title,
-                   xlabel=xlabel, ylabel=ylabel, yscale='log', xoffset=xoffset, 
-                   filename=f'pairwise-graphs/{spl}-{pair[0]}-{pair[1]}.png', colors=color)
+                   xlabel=xlabel, ylabel=ylabel, yscale=yscale, xoffset=xoffset, 
+                   filename=f'{output_path}/pairwise-graphs/{spl}-{pair[0]}-{pair[1]}-rt.png', colors=color)
 
        mem_filenames = [
            'csv/memory_usage/totalMemory{}{}.csv'.format(spl, label) for label in labels]
@@ -35,25 +35,27 @@ def get_pairwise_graphs(spl, labels, xoffset=0):
        xlabel = 'Evolution'
        ylabel = 'Memory Usage (MB)'
        make_line_graph(mem_df, spl, pair, title=title,
-                   xlabel=xlabel, ylabel=ylabel, yscale='log', xoffset=xoffset,
-                   filename=f'pairwise-graphs/{spl}-{pair[0]}-{pair[1]}.png', colors=color)
+                   xlabel=xlabel, ylabel=ylabel, yscale=yscale, xoffset=xoffset,
+                   filename=f'{output_path}/pairwise-graphs/{spl}-{pair[0]}-{pair[1]}-mem.png', colors=color)
 
-def plot_spl(spl, labels, xoffset=0):
+def plot_spl(spl, labels, xoffset=0, yscale='log', output_path='results'):
     display(Markdown('# {}'.format(spl)))
     rt_filenames = [
         'csv/running_time/totalTime{}{}.csv'.format(spl, label) for label in labels]
     factor = 1e-3
     rt_df = read_data(spl, rt_filenames, labels, factor=factor)
     process(rt_df, spl, labels, '{}: Running Time'.format(spl), header='Running Time',
-            ylabel='Running Time (s)', xoffset=xoffset, factor=1.0/1000.0, table_suffix='rt',
-            table_description="Average running time (s) (statistically smallest value in bold)")
+            ylabel='Running Time (s)', yscale=yscale, xoffset=xoffset, factor=1.0/1000.0, file_suffix='rt',
+            table_description="Average running time (s) (statistically smallest value in bold)",
+            output_path=output_path)
 
     mem_filenames = [
         'csv/memory_usage/totalMemory{}{}.csv'.format(spl, label) for label in labels]
     mem_df = read_data(spl, mem_filenames, labels)
     process(mem_df, spl, labels, '{}: Memory Usage'.format(spl), header='Memory Usage',
-            ylabel='Memory Usage (MB)', xoffset=xoffset, table_suffix='mem',
-            table_description="Average memory usage (MB) (statistically smallest value in bold)")
+            ylabel='Memory Usage (MB)', yscale=yscale, xoffset=xoffset, file_suffix='mem',
+            table_description="Average memory usage (MB) (statistically smallest value in bold)",
+            output_path=output_path)
 
     l1 = labels[0]
     l2 = labels[1]
@@ -65,25 +67,27 @@ def plot_spl(spl, labels, xoffset=0):
         spl, rt_df, mem_df, l1, l2, suffix1='Runtime (s)', suffix2='Memory Usage (MB)', errors1=True, errors2=False, idx_offset=xoffset)
     display(test_df)
     pd.set_option('precision', 2)
-    with open('tables/effect-size/{}.md'.format(spl), 'w') as f:
+    with open(f'{output_path}/tables/effect-size/{spl}.md', 'w') as f:
         test_df = get_test_comparison_dfs(spl, rt_df, mem_df, l1, l2, suffix1='Runtime (s)',
                                           suffix2='Memory Usage (MB)', errors1=True, errors2=False, formatting='markdown', idx_offset=xoffset)
         f.write(test_df.to_markdown(index=False))
-    with open('tables/effect-size/{}.tex'.format(spl), 'w') as f:
+    with open(f'{output_path}/tables/effect-size/{spl}.tex', 'w') as f:
         test_df = get_test_comparison_dfs(spl, rt_df, mem_df, l1, l2, suffix1='Runtime (s)',
                                           suffix2='Memory Usage (MB)', errors1=True, errors2=False, formatting='latex', idx_offset=xoffset)
         f.write(test_df.to_latex(index=False, escape=False))
 
-def process(df, spl, labels, title, header=None, xlabel='Evolution', ylabel='', yscale='log', xoffset=1, factor=1.0, table_suffix='', table_description=None):
+def process(df, spl, labels, title, header=None, xlabel='Evolution',
+            ylabel='', yscale='log', xoffset=1, factor=1.0,
+            file_suffix='', table_description=None, output_path='results'):
     if header:
         display(Markdown('## {}'.format(header)))
     make_line_graph(df, spl, labels, title=title,
-                    xlabel=xlabel, ylabel=ylabel, yscale='log', xoffset=xoffset,
-                    filename=f'graphs/{spl}.png')
+                    xlabel=xlabel, ylabel=ylabel, yscale=yscale, xoffset=xoffset,
+                    filename=f'{output_path}/graphs/{spl}{file_suffix}.png')
 
     test_results = test_all_evolutions_pairs(df, labels)
 
-    make_box_plot(df, spl, title=title, filename=f'boxplots/{spl}.png')
+    make_box_plot(df, spl, title=title, filename=f'{output_path}/boxplots/{spl}{file_suffix}.png')
 
     orderings = get_orderings(test_results)
 
@@ -104,5 +108,5 @@ def process(df, spl, labels, title, header=None, xlabel='Evolution', ylabel='', 
 
     display(Markdown(table))
 
-    with open('tables/{}{}.md'.format(spl, table_suffix), 'w') as f:
+    with open(f'{output_path}/tables/{spl}{file_suffix}.md', 'w') as f:
         f.write(table)
